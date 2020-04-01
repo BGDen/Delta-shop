d7313d912f61e863f9519d6253ae5aef39bd0628
Den <fentau.line@gmail.com>
2020-04-01 20:56:56 +0300

    init commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,6 @@ export function Navigation (props){
 
       <div className="form-inline my-2 my-lg-0">
         <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
       </div>
     </nav>
   )
