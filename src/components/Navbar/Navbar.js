4c174f06e8586724eb6cb307aa4f18a4dae4d77b
Den <fentau.line@gmail.com>
2020-04-01 20:53:03 +0300

    Create a React app and publish it to GitHub Pages

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export function Navigation (props){
+  // console.log(props);
+  return(
+    <nav className="navbar navbar-light bg-light">
+      
+      <a className="navbar-brand" href="#">Navbar</a>
+
+      <div className="form-inline my-2 my-lg-0">
+        <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
+      </div>
+    </nav>
+  )
+}
